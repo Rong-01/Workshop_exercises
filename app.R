7bcaa72319c40a1e28f3437a77e867d646daa410
Rong-01 <48717613+Rong-01@users.noreply.github.com>
2025-08-19 17:21:03 +0200

    Update app.R

--- app.R
+++ app.R
@@ -3,7 +3,7 @@ library(rsconnect)
 rsconnect::setAccountInfo(name='rong0001', token='A31BFAAB3A1BD63AD1C93941CC41C5DF', secret='tdLPYIIzjtUMWjRQXdgYlC7UTWdNg86O/9dB+JEF')
 
 # once rsconnect is configured, deploy you shiny app
-rsconnect::deployApp("Exercise_1", appName = "exercise_1_new")
+rsconnect::deployApp("Exercise_1", appName = "exercise_1_new", forceUpdate = TRUE)
 # rsconnect::deployApp("Exercise_1", forceUpdate = TRUE)
 #deployApp()
 
@@ -12,6 +12,7 @@ library(shiny)
 runApp()
 
 #rsconnect::writeManifest()
+
 
 
 
